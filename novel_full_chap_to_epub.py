415c920dccb2333dc37e19ec10ba70261c84f0f0
ChrisYeomans <chrisj.yeomans@gmail.com>
2025-05-14 22:07:49 +0100

    swiched to using pypub

--- novel_full_chap_to_epub.py
+++ novel_full_chap_to_epub.py
@@ -1,13 +1,13 @@
 from bs4 import BeautifulSoup as BS
-from ebooklib import epub
+import pypub
 import requests
 
 from typing import List
 
 class NovelFullBook:
     entry: str = ""
-    book: epub.EpubBook = epub.EpubBook()
-    chapters: list[epub.EpubHtml] = []
+    book: pypub.Epub = pypub.Epub("")
+    chapters: list[any] = []
     chapter_titles: dict[str, int] = {}
 
     def __init__(self, entry: str):
@@ -18,12 +18,8 @@ class NovelFullBook:
         s: BS = BS(p, 'html.parser')
 
         title: str = s.find("h3", { "class" : "title" }).text
-        self.book.set_identifier(''.join([e for e in title if e != ' ']))
-        self.book.set_title(title)
-        self.book.set_language("en")
-
         author: str = s.find("div", { "class" : "info" }).find("a").text
-        self.book.add_author(author)
+        self.book = pypub.Epub(title, creator=author)
 
     def gen_chapter(self, link: str) -> str:
         if not link:
@@ -38,36 +34,32 @@ class NovelFullBook:
             chap_title: str = chap_title+'('+str(self.chapter_titles[chap_title])+')'
         else:
             self.chapter_titles[chap_title] = 0
-        stripped_chap_title: str = ''.join(chap_title.split())
-        c: epub.EpubHtml = epub.EpubHtml(title=chap_title, file_name=stripped_chap_title+".xhtml")
-        c.content = (str(a).encode('utf-8'))
-        self.book.add_item(c)
-
-        self.book.toc.append(
-            epub.Link(stripped_chap_title+".xhtml", chap_title, chap_title),
-        )
+        c: pypub.Chapter = pypub.create_chapter_from_text(a.text, title=chap_title)
+        ass: pypub.Assignment = self.book.assign_chapter()
+        self.book.builder.render_chapter(ass, c)
+        # self.book.add_chapter(c)
         self.chapters.append(c)
 
         next_chap_link: str = s.find("a", { "id" : "next_chap" }).get('href')
         return "https://novelfull.net" + next_chap_link if next_chap_link else ""
         
     def write_book(self):
-        self.book.add_item(epub.EpubNcx())
-        self.book.add_item(epub.EpubNav())
-        self.book.spine = ["nav"] + self.chapters
-        epub.write_epub(self.book.title + ".epub", self.book, {})
+        self.book.builder.finalize(self.book.title + ".epub")
 
 
-PAGE: str = "https://novelfull.net/cursed-immortality.html"
-c1: str = "https://novelfull.net/cursed-immortality/chapter-1-wish-of-immortality.html"
+PAGE: str = "https://novelfull.net/a-villains-will-to-survive.html"
+c1: str = "https://novelfull.net/a-villains-will-to-survive/chapter-1-prologue.html"
 
 a: NovelFullBook = NovelFullBook(PAGE)
 a.set_metadata()
-l: str = a.gen_chapter(c1)
-while 1:
-    if l:
-        l = a.gen_chapter(l)
-        print(l)
-    else:
-        break
-a.write_book()
+
+with a.book.builder as builder:
+    dirs = builder.begin()
+    l: str = a.gen_chapter(c1)
+    while 1:
+        if l:
+            l = a.gen_chapter(l)
+            print(l)
+        else:
+            break
+    a.write_book()
